22601b3da775a27261ea5939562517b0eaf3c4e8
alensign <alexandra.wilchitskaya@gmail.com>
2022-06-28 18:21:34 +0300

    upd settings

--- dj_sample_project/testsite/testsite/settings.py
+++ dj_sample_project/testsite/testsite/settings.py
@@ -21,7 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3n_*xe%x%5qsf9=qqxp%@n@=c8h8j4h2bsq%#%^jlbw2$(5%!8'
+import environ
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False))
+# reading .env file
+environ.Env.read_env()
+...
+#  We are ready to change the DEBUG, SECRET_KEY and DATABASES lines to read their values from the environment.
+DEBUG = env('DEBUG')
+...
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
